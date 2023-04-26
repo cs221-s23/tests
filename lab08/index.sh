f807c1808b5d2c972e4569ab4bc89962883efece
Eunjin Jung <ejung@vlab05.cs.usfca.edu>
2023-04-25 21:58:50 -0700

    #! added

--- lab08/index.sh
+++ lab08/index.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env sh
 export PORT=$(cat port.txt); wget localhost:$PORT/ -O index.html
--- lab08/start.sh
+++ lab08/start.sh
@@ -1,1 +1,3 @@
-rm *.tmp; export PORT=$(cat port.txt); (./lab08 -p $PORT &); echo "started" > started.tmp
+#!/usr/bin/env sh
+rm *.tmp; export PORT=$(cat port.txt); (./lab08 -p $PORT &); echo "started" > started.tmp 
+#; wget localhost:$PORT/ -O index.html; pkill lab08;  echo "stopped" > stopped.tmp
